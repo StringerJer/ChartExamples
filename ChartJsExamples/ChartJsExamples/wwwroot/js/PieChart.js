a6716549db75aa3f83945832efafb403f06968ed
StringerJer <89177753+StringerJer@users.noreply.github.com>
2022-06-04 02:34:05 -0400

    Make Pie chart more dynamic.

--- ChartJsExamples/ChartJsExamples/wwwroot/js/PieChart.js
+++ ChartJsExamples/ChartJsExamples/wwwroot/js/PieChart.js
@@ -1,5 +1,5 @@
 ﻿(
-    async function getPieChart() {
+    async () => {
         let apiData = await getApiData();
         const branches = apiData.map(o => o.branch);
         const revenues2020 = apiData.map(o => o.revenue2020);
@@ -7,44 +7,54 @@
         const revenues2018 = apiData.map(o => o.revenue2018);
         const colorPool = ["purple", "red", "green", "yellow", "blue"]
 
-        const ctx3 = document.getElementById('myChart3');
+        let listOfCharts = document.getElementsByTagName("canvas"); //.getElementById('myChart3');
 
-        new Chart(ctx3, {
-            type: 'pie',
-            data: {
-                labels: branches,
-                datasets: [
-                    {
-                        backgroundColor: colorPool,
-                        label: "Revenue 2018",
-                        data: revenues2018
-                    },
-                    {
-                        backgroundColor: colorPool,
-                        label: "Revenue 2019",
-                        data: revenues2019
-                    },
-                    {
-                        backgroundColor: colorPool,
-                        label: "Revenue 2020",
-                        data: revenues2020
-                    }
-                ]
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: "right"
-                    }
+        for (canvas of listOfCharts) {
+            if (canvas.dataset.year) {
+                switch (canvas.dataset.year) {
+                    case "2018": createChart(canvas, revenues2018);
+                        break;
+                    case "2019": createChart(canvas, revenues2019);
+                        break;
+                    case "2020": createChart(canvas, revenues2020);
+                        break;
+                    default: break;
                 }
             }
-        });
+        }
 
         async function getApiData() {
             const apiResult = await fetch("https://chartjsapi.azurewebsites.net/branch/revenues");
             const json = await apiResult.json();
             return json;
+        }
+
+        function clickHandler(evt) {
+
+        }
+
+        function createChart(canvas, data) {
+            new Chart(canvas, {
+                type: 'pie',
+                data: {
+                    labels: branches,
+                    datasets: [{
+                        backgroundColor: colorPool,
+                        data: data
+                    }, {
+                            backgroundColor: colorPool,
+                            data: data
+                        }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: "right"
+                        }
+                    }
+                }
+            });
         }
     }
 )();
--- ChartJsExamples/ChartJsExamples/wwwroot/js/site.js
+++ ChartJsExamples/ChartJsExamples/wwwroot/js/site.js
@@ -3,106 +3,106 @@
 
 // Write your JavaScript code.
 
-(
-    async () => {
-        let apiData = await getApiData();
-        const branches = apiData.map(o => o.branch);
-        const revenues2020 = apiData.map(o => o.revenue2020);
-        const revenues2019 = apiData.map(o => o.revenue2019);
-        const revenues2018 = apiData.map(o => o.revenue2018);
-        const colorPool = ["purple", "red", "green", "yellow", "blue"]
+//(
+//    async () => {
+//        let apiData = await getApiData();
+//        const branches = apiData.map(o => o.branch);
+//        const revenues2020 = apiData.map(o => o.revenue2020);
+//        const revenues2019 = apiData.map(o => o.revenue2019);
+//        const revenues2018 = apiData.map(o => o.revenue2018);
+//        const colorPool = ["purple", "red", "green", "yellow", "blue"]
 
-        const ctx = document.getElementById('myChart');
-        const ctx1 = document.getElementById('myChart1');
-        const ctx2 = document.getElementById('myChart2');
+//        const ctx = document.getElementById('myChart');
+//        const ctx1 = document.getElementById('myChart1');
+//        const ctx2 = document.getElementById('myChart2');
 
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: branches,
-                datasets: [
-                    {
-                        backgroundColor: "green",
-                        label: "Revenue 2018",
-                        data: revenues2018
-                    },
-                    {
-                        backgroundColor: "red",
-                        label: "Revenue 2019",
-                        data: revenues2019
-                    },
-                    {
-                        backgroundColor: "purple",
-                        label: "Revenue 2020",
-                        data: revenues2020
-                    }
-                ]
-            },
-            options: {
-                responsive: true
-            }
-        });
+//        new Chart(ctx, {
+//            type: 'bar',
+//            data: {
+//                labels: branches,
+//                datasets: [
+//                    {
+//                        backgroundColor: "green",
+//                        label: "Revenue 2018",
+//                        data: revenues2018
+//                    },
+//                    {
+//                        backgroundColor: "red",
+//                        label: "Revenue 2019",
+//                        data: revenues2019
+//                    },
+//                    {
+//                        backgroundColor: "purple",
+//                        label: "Revenue 2020",
+//                        data: revenues2020
+//                    }
+//                ]
+//            },
+//            options: {
+//                responsive: true
+//            }
+//        });
 
-        new Chart(ctx1, {
-            type: 'bar',
-            data: {
-                labels: branches,
-                datasets: [
-                    {
-                        backgroundColor: "green",
-                        label: "Revenue 2018",
-                        data: revenues2018
-                    },
-                    {
-                        backgroundColor: "red",
-                        label: "Revenue 2019",
-                        data: revenues2019
-                    },
-                    {
-                        backgroundColor: "purple",
-                        label: "Revenue 2020",
-                        data: revenues2020
-                    }
-                ]
-            },
-            options: {
-                indexAxis: 'y',
-                responsive: true
-            }
-        });
+//        new Chart(ctx1, {
+//            type: 'bar',
+//            data: {
+//                labels: branches,
+//                datasets: [
+//                    {
+//                        backgroundColor: "green",
+//                        label: "Revenue 2018",
+//                        data: revenues2018
+//                    },
+//                    {
+//                        backgroundColor: "red",
+//                        label: "Revenue 2019",
+//                        data: revenues2019
+//                    },
+//                    {
+//                        backgroundColor: "purple",
+//                        label: "Revenue 2020",
+//                        data: revenues2020
+//                    }
+//                ]
+//            },
+//            options: {
+//                indexAxis: 'y',
+//                responsive: true
+//            }
+//        });
 
-        new Chart(ctx2, {
-            type: 'bar',
-            data: {
-                labels: branches,
-                datasets: [
-                    {
-                        backgroundColor: "green",
-                        label: "Revenue 2018",
-                        data: revenues2018
-                    },
-                    {
-                        backgroundColor: "red",
-                        label: "Revenue 2019",
-                        data: revenues2019
-                    },
-                    {
-                        backgroundColor: "purple",
-                        label: "Revenue 2020",
-                        data: revenues2020
-                    }
-                ]
-            },
-            options: {
-                indexAxis: 'y',
-                responsive: true
-            }
-        });
+//        new Chart(ctx2, {
+//            type: 'bar',
+//            data: {
+//                labels: branches,
+//                datasets: [
+//                    {
+//                        backgroundColor: "green",
+//                        label: "Revenue 2018",
+//                        data: revenues2018
+//                    },
+//                    {
+//                        backgroundColor: "red",
+//                        label: "Revenue 2019",
+//                        data: revenues2019
+//                    },
+//                    {
+//                        backgroundColor: "purple",
+//                        label: "Revenue 2020",
+//                        data: revenues2020
+//                    }
+//                ]
+//            },
+//            options: {
+//                indexAxis: 'y',
+//                responsive: true
+//            }
+//        });
 
-        async function getApiData() {
-            const apiResult = await fetch("https://chartjsapi.azurewebsites.net/branch/revenues");
-            const json = await apiResult.json();
-            return json;
-        }
-    }    
-)();
+//        async function getApiData() {
+//            const apiResult = await fetch("https://chartjsapi.azurewebsites.net/branch/revenues");
+//            const json = await apiResult.json();
+//            return json;
+//        }
+//    }    
+//)();
